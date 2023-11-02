ac410da0b14a9bdf8f02953326d8fab5f8852f4f
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-01 20:25:12 -0700

    feat(landing): Conference component

---
+++ src/components/Conference/Conference.tsx
@@ -0,0 +1,24 @@
+import Timer from '../Timer/Timer';
+
+const Conference = () => {
+  return (
+    <section>
+      <div className='max-w-7xl mx-auto'>
+        <div className='px-4 space-y-8 py-20'>
+          <h3 className=' font-bold text-2xl text-blue-500 font-barlow'>
+            Conferencia Especial de Nuestro CEO: <br />
+            Gana dinero sin poner en riesgo tu propio Dinero
+          </h3>
+          <p className='text-blue-600 text-lg leading-6 font-barlow'>
+            Únete a nosotros en nuestro gran evento de aniversario y descubre
+            todas las herramientas e innovaciones en las que hemos trabajado en
+            Impulse para que puedas convertirte en un trader profesional y
+            rentable utilizando el capital de nuestra compañía.
+          </p>
+          <Timer />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Conference;
