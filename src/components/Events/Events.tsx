42e07b9b2fff0a0b7e54873b8e599175ffdf2cf0
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-02 10:30:21 -0700

    feat: Events component

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -4,18 +4,37 @@ const Events = () => {
   return (
     <section>
       <div className='max-w-7xl mx-auto'>
-        <div className='px-4 space-y-8 py-20'>
-          <div className='flex flex-col items-center'>
+        <div className='px-4 space-y-8 pb-20 pt-4'>
+          <div className='flex flex-col items-center font-barlow'>
             <Icon src='./calendar.png' alt='calendarImage' />
-            <span className='text-blue-500 font-semibold text-xl uppercase tracking-tight'>
+            <span className='text-blue-400 font-medium text-xl uppercase'>
               SABADO
             </span>
-            <span className='text-blue-500 font-semibold text-3xl uppercase tracking-tighter'>
+            <span className='text-blue-400 font-medium font-sans text-3xl uppercase tracking-tight leading-7'>
               30 septiembre
             </span>
           </div>
-          <Icon src='./timer.png' alt='timerImage' />
-          <Icon src='./monitor.png' alt='monitorImage' />
+          <div className='flex flex-col items-center '>
+            <Icon src='./timer.png' alt='timerImage' />
+            <span className='text-blue-400 font-semibold text-2xl'>
+              13:00 hrs
+            </span>
+            <span className='text-blue-400 font-semibold text-2xl'>
+              Argentina
+            </span>
+          </div>
+          <div className='flex flex-col items-center'>
+            <Icon src='./monitor.png' alt='monitorImage' />
+            <span className='text-blue-400 font-semibold text-2xl'>
+              Evento digital{' '}
+              <span className='uppercase font-barlow font-medium  text-[1.3rem]'>
+                GRATUITO
+              </span>
+            </span>
+            <span className='text-blue-400 font-semibold text-xl'>
+              En vivo desde YouTube
+            </span>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Events/components/Icon.tsx
+++ src/components/Events/components/Icon.tsx
@@ -6,7 +6,7 @@ type IconProps = {
 const Icon: React.FC<IconProps> = ({ src, alt }) => {
   return (
     <div className='w-28'>
-      <img src={src} alt={alt} className='w-full mb-10' />
+      <img src={src} alt={alt} className='w-full my-7' />
     </div>
   );
 };
