2f852a0e800b619465eddadd55a50c44dbd7cb75
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-02 10:37:22 -0700

    feat(landing): Register component responsive

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -3,7 +3,7 @@ const Register = () => {
     <section className='background'>
       <div className='max-w-7xl mx-auto'>
         <div className='flex flex-col px-4 space-y-10 py-12 md:flex-row'>
-          <div className='space-y-6'>
+          <div className='space-y-6 md:max-w-3xl'>
             <h3 className='text-[1.8rem] leading-9 font-semibold text-white font-barlow '>
               Al registrarte hoy, llévate gratis nuestro{' '}
               <span className='whitespace-nowrap'>E-Book</span> “Cómo ganar
@@ -13,7 +13,7 @@ const Register = () => {
               Que incluye un capítulo de regalo para desmantelar sesgos y
               maximizar las probabilidades favorables de tu trading.
             </p>
-            <div className='w-full flex justify-center pt-3'>
+            <div className='w-full flex justify-center pt-3 md:justify-start'>
               <button className='bg-blue-600 font-barlow text-white uppercase rounded-full px-20 py-4 font-semibold hover:bg-blue-500 tracking-widest'>
                 ¡Registrate Ahora!
               </button>
