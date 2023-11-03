483223ac555e1ce8450bf5ec0d776518191ebca3
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-11-02 22:55:07 -0600

    responsivo

--- src/components/Conference/Conference.tsx
+++ src/components/Conference/Conference.tsx
@@ -1,36 +1,61 @@
 import Timer from "../Timer/Timer";
+import { RiSendPlane2Fill } from "react-icons/ri";
 
 const Conference = () => {
   return (
     <section>
       <div className="max-w-7xl mx-auto">
-        <div className="px-4 space-y-8 py-20 md:px-20">
+        <div className="px-4 space-y-8 py-20 md:px-20 text-black">
           <h3
-            className="text-[60px] font-bold text-blue-600 text-center underline underline-offset-8"
+            className="text-[40px] md:text-[60px] font-bold text-center underline underline-offset-8"
             style={{ fontFamily: "source-code" }}
           >
-            ¿QUIÉN ES SAÚL ZAVALA?
+            ¿QUIÉN ES <span className="whitespace-nowrap">SAÚL ZAVALA</span>?
             <br />
           </h3>
-          <span className="w-full text-[60px] font-bold text-blue-600 text-center"></span>
 
-          <ul
-            className="text-blue-600 text-3xl font-medium"
-            style={{
-              listStyle: "disc",
-            }}
-          >
-            <li>FUNDADOR Y CEO TOP X</li>
-            <li className="mt-[20px]">
-              CREADOR DEL PROYECTO MAS AMBICIOSO DE LA INDUSTRIA DEL NETWORK
-              MARKETING “LA ERA DORADA DEL MULTINIVEL”
+          <ul className="text-xl md:text-3xl font-medium">
+            <li className="flex items-center space-x-2">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>FUNDADOR Y CEO TOP X</span>
             </li>
-            <li className="mt-[20px]">7 CIFRAS A SUS 20´S </li>
-            <li className="mt-[20px]">AUTOR BESTSELLER DEL LIBRO “OBSESION”</li>
-            <li className="mt-[20px]">MAS DE 6 AÑOS DE EXPERIENCIA EN MLM</li>
-            <li className="mt-[20px]">
-              SPEAKER INTERNACIONAL CERTIFICADO CREADOR DE ORGANIZACIONES A
-              NIVEL MUNDIAL
+            <li className="flex items-center space-x-2 mt-[20px]">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>
+                CREADOR DEL PROYECTO MAS AMBICIOSO DE LA INDUSTRIA DEL NETWORK
+                MARKETING “LA ERA DORADA DEL MULTINIVEL”
+              </span>
+            </li>
+            <li className="flex items-center space-x-2 mt-[20px]">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>7 CIFRAS A SUS 20´S</span>
+            </li>
+            <li className="flex items-center space-x-2 mt-[20px]">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>AUTOR BESTSELLER DEL LIBRO “OBSESION”</span>
+            </li>
+            <li className="flex items-center space-x-2 mt-[20px]">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>MAS DE 6 AÑOS DE EXPERIENCIA EN MLM</span>
+            </li>
+            <li className="flex items-center space-x-2 mt-[20px]">
+              <RiSendPlane2Fill
+                style={{ display: "inline", minWidth: 30, color: "#a67c00" }}
+              />
+              <span>
+                SPEAKER INTERNACIONAL CERTIFICADO CREADOR DE ORGANIZACIONES A
+                NIVEL MUNDIAL
+              </span>
             </li>
           </ul>
           <br />
--- src/components/Timer/components/Count.tsx
+++ src/components/Timer/components/Count.tsx
@@ -6,10 +6,10 @@ type Props = {
 const Count: React.FC<Props> = ({ value, label }) => {
   return (
     <div className='flex flex-col items-center'>
-      <span className='text-blue-500 font-medium text-2xl md:text-6xl md:font-bold md:pt-10'>
+      <span className='text-black font-medium text-2xl md:text-6xl md:font-bold md:pt-10'>
         {value}
       </span>
-      <span className='text-blue-500 uppercase'>{label}</span>
+      <span className='text-black uppercase'>{label}</span>
     </div>
   );
 };
