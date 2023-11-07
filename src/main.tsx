e85053437ee737b51f79359ce3ccc2eb094bc50f
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-11-07 12:24:53 -0700

    React router added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ThankYou from './components/ThankYou/ThankYou.component.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/thanks" element={<ThankYou />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
